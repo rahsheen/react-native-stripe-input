82e9938351d3e943ef2c4f6858fdf781ed997beb
Rahsheen Porter <rahsheen.porter@gmail.com>
2018-10-14 17:49:15 -0400

    feat(ios): Add PaymentCardTextField and link to JS

---
+++ ios/RNSTPInputManager.h
@@ -0,0 +1,13 @@
+//
+//  RNSTPinputManager.h
+//  testproject
+//
+//  Created by Rahsheen Porter on 10/14/18.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+#import <React/RCTViewManager.h>
+#import <Stripe/Stripe.h>
+
+@interface RNSTPInputManager : RCTViewManager
+@end
